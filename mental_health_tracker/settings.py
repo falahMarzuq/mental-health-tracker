186aba8e908ebcd26ac02dfc6dd4ba1c7b1dc6a2
falahMarzuq <muhammadfalahmarzuq@gmail.com>
2024-08-28 16:20:33 +0700

    added URL deployment pws to allowed_hosts

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0ky5qu)d8t!y*6-_h!$95!w=ky5e@o(xj&9t%0145ps_7td0m)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-falah-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
